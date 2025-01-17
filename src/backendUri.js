615d9e951e77da62e6ac41ba08c544453614a96b
Nenad005 <markonenadovic2005@gmail.com>
2025-01-17 04:17:42 +0100

    new strapi

--- src/backendUri.js
+++ src/backendUri.js
@@ -1,1 +1,3 @@
-export const backendUri = 'https://vs.sveti-sava.edu.rs'
+// export const backendUri = 'https://vs.sveti-sava.edu.rs'
+// export const backendUri = 'https://portfolio-strapi-qszr.onrender.com/'
+export const backendUri = "https://portfolio-strapi-qszr.onrender.com"
